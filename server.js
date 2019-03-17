cc4298e7452d9fd6de2dc993246465d9cd088a88
gezahegnw <gezahegnw@gmail.com>
2019-03-17 16:17:12 -0500

    push it to heroku

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var passport = require("./config/passport");
 var socket = require('socket.io');
 // Requiring our models for syncing
 var db = require("./models");
+//=======================================================
+const nodemailer = require('nodemailer');
+
 
 // =============================================================
 var PORT = process.env.PORT || 8080;
@@ -97,6 +100,54 @@ io.sockets.on('connection', function(socket) {
   
 });
 //gg
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
 
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smpt.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'gezahegnw@gmail.com', // generated ethereal user
+        pass: 'wemet2001'  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Nodemailer Contact" <gezahegnw@email.com>', // sender address
+      to: 'gezahegnw@gmail.com', // list of receivers
+      subject: 'Node Contact Request', // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', {msg:'Email has been sent'});
+  });
+  });
+//g
 });
 module.exports = app;

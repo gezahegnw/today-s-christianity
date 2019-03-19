c89ad95f8580848fc70aef4509b8375cfee75c67
gezahegnw <gezahegnw@gmail.com>
2019-03-18 19:06:25 -0500

    commented out nodemailer

--- server.js
+++ server.js
@@ -105,60 +105,60 @@ db.sequelize.sync(syncOptions).then(function () {
   });
   //gg
   
-  app.use('/public', express.static(path.join(__dirname, 'public')));
-  app.get('/', (req, res) => {
-    res.render('contact');
-  });
-  app.post('/send', (req, res) => {
-    const output = `
-    <p>You have a new contact request from your BLOGGER APP!</p>
-    <h3>Contact Details</h3>
-    <ul>  
-      <li>Name: ${req.body.name}</li>
-      <li>Company: ${req.body.company}</li>
-      <li>Email: ${req.body.email}</li>
-      <li>Phone: ${req.body.phone}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</p>
-  `;
+  // app.use('/public', express.static(path.join(__dirname, 'public')));
+  // app.get('/', (req, res) => {
+  //   res.render('contact');
+  // });
+  // app.post('/send', (req, res) => {
+  //   const output = `
+  //   <p>You have a new contact request from your BLOGGER APP!</p>
+  //   <h3>Contact Details</h3>
+  //   <ul>  
+  //     <li>Name: ${req.body.name}</li>
+  //     <li>Company: ${req.body.company}</li>
+  //     <li>Email: ${req.body.email}</li>
+  //     <li>Phone: ${req.body.phone}</li>
+  //   </ul>
+  //   <h3>Message</h3>
+  //   <p>${req.body.message}</p>
+  // `;
  
 
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    // host: 'smpt.gmail.com',
-    // port: 587,
-    // secure: false, // true for 465, false for other ports
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL, // generated ethereal user
-        pass: process.env.PASSWORD  // generated ethereal password
-    },
-    tls:{
-      rejectUnauthorized:false
-    }
-  });
+  // // create reusable transporter object using the default SMTP transport
+  // let transporter = nodemailer.createTransport({
+  //   // host: 'smpt.gmail.com',
+  //   // port: 587,
+  //   // secure: false, // true for 465, false for other ports
+  //   service: 'gmail',
+  //   auth: {
+  //       user: process.env.EMAIL, // generated ethereal user
+  //       pass: process.env.PASSWORD  // generated ethereal password
+  //   },
+  //   tls:{
+  //     rejectUnauthorized:false
+  //   }
+  // });
 
-  // setup email data with unicode symbols
-  let mailOptions = {
-      from: req.body.email, //'"Nodemailer Contact" <gezaworku@gmail.com>', // sender address
-      to: 'gezahegnw@gmail.com', // list of receivers
-      subject: 'This EMAIL from my BLOGGER APP', // Subject line
-      text: 'Hello world?', // plain text body
-      html: output // html body
-  };
+  // // setup email data with unicode symbols
+  // let mailOptions = {
+  //     from: req.body.email, //'"Nodemailer Contact" <gezaworku@gmail.com>', // sender address
+  //     to: 'gezahegnw@gmail.com', // list of receivers
+  //     subject: 'This EMAIL from my BLOGGER APP', // Subject line
+  //     text: 'Hello world?', // plain text body
+  //     html: output // html body
+  // };
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);   
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  // // send mail with defined transport object
+  // transporter.sendMail(mailOptions, (error, info) => {
+  //     if (error) {
+  //         return console.log(error);
+  //     }
+  //     console.log('Message sent: %s', info.messageId);   
+  //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-      res.render('contact', {msg:'Email has been sent'});
-  });
-  });
+  //     res.render('contact', {msg:'Email has been sent'});
+  // });
+  // });
 });
 module.exports = app;
